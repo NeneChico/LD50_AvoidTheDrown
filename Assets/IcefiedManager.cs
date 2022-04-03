3e5aa379a1a99c2006c762de642c9990ed75032c
Jean-Paul Carmona <jpcarmona@jpca.fr>
2022-04-03 11:05:20 +0200

    dive and emerging management

--- Assets/IcefiedManager.cs
+++ Assets/IcefiedManager.cs
@@ -6,20 +6,28 @@ public class IcefiedManager : MonoBehaviour
 {
 
     [SerializeField]
-    protected float sizeEntropy = 0.1f;
+    protected float entropy = 0.1f;
 
     [SerializeField]
-    protected float resizeEntropy = 0.1f;
-
-    [SerializeField]
-    protected float FloatAmplitude;
+    protected float FloatAmplitude = 2;
 
     [SerializeField]
     protected float FloatSpeed;
-    
-    protected float tempFloatVal;
 
+    [SerializeField]
+    protected float DivingScaleSize = 5f;
+
+    // emerging management
+    protected Vector3 emergeVelocity = Vector3.zero;
+    protected float emergeSpeedSeconds = 1f;
+
+    // floating management
+    protected float tempFloatVal;
     protected Vector3 tempPos;
+
+    // diving management
+    protected Vector3 diveVelocity = Vector3.zero;
+    protected float diveSpeedSeconds = 0.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -42,16 +50,31 @@ public class IcefiedManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // TODO: tilt from player weight
-        // TODO: move to player direction
-
-        if(gameObject.transform.localScale.x <= 0.01f)
+        // dive ?
+        if (gameObject.transform.localScale.x <= DivingScaleSize)
         {
-            Destroy(gameObject);
+            Debug.Log($"Diving {gameObject}");
+            Vector3 targetPosition = new Vector3(gameObject.transform.position.x, -3, gameObject.transform.position.z);
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref diveVelocity, diveSpeedSeconds);
         }
+        else
+        {
+            // emerge
+            if (transform.position.y < 0)
+            {
+                Debug.Log($"Emerging {gameObject}");
+                Vector3 targetPosition = new Vector3(gameObject.transform.position.x, 0, gameObject.transform.position.z);
+                transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref emergeVelocity, emergeSpeedSeconds);
+            }
+            else
+            {
+                // TODO: tilt from player weight
+                // TODO: move to player direction
 
-        // floating movement
-        tempPos.y = tempFloatVal + FloatAmplitude * Mathf.Sin(FloatSpeed * Time.time);
-        transform.position = tempPos;
+                // floating movement
+                tempPos.y = tempFloatVal + FloatAmplitude * Mathf.Sin(FloatSpeed * Time.time);
+                transform.position = tempPos;
+            }
+        }
     }
 }
